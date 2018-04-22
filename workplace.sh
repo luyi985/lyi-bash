29f75f348c94beb55661db0b82ec73945e1a090c
Yi Lu <luyi985@gmail.com>
2018-04-22 03:38:34 -0700

    small fix

--- workplace.sh
+++ workplace.sh
@@ -1,13 +1,12 @@
-function loadPython() {
+loadPython() {
 	[ -f ~/git/python-learn/python.sh ] || echo "No Python.sh found at ${~/git/python-learn/python.sh}"
 	[ -f ~/git/python-learn/python.sh ] || exit 1;
 	[ -f ~/git/python-learn/python.sh ] && source ~/git/python-learn/python.sh
 }
 
 editBash() {
-	subl -a $LYI_BASH || code -a $LYI_BASH
-	[[ -f "${HOME}/.bashrc" ]] && { subl -a ${HOME}/.bashrc || code -a ${HOME}/.bashrc; }
-	[[ -f "${HOME}/.bash_profile" ]] && { subl -a ${HOME}/.bash_profile || code -a ${HOME}/.bash_profile; }
+	[[ -f "${HOME}/.bashrc" ]] && lyiEdit "${HOME}/.bashrc"
+	[[ -f "${HOME}/.bash_profile" ]] && lyiEdit "${HOME}/.bash_profile"
 	[[ $1 == 'h' ]] && cd $LYI_BASH
 }
 
