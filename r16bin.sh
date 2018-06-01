d687897d4274999b6015fc6c3d362d438ee139aa
luyi985 <luyi985@gmail.com>
2018-06-01 13:02:17 +1000

    upgrad recat16

---
+++ r16bin.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+PACKPATH=(
+	"/home/bxmdev/git/flux/src"
+)
+OPS=(
+	"/react-codemod/transforms/React-PropTypes-to-prop-types.js"
+	"/react-codemod/transforms/class.js --mixin-module-name=react-addons-pure-render-mixin --flow=true --pure-component=true --remove-runtime-proptypes=false"
+	"/react-codemod/transforms/findDOMNode.js"
+	"/react-codemod/transforms/react-to-react-dom.js"
+)
+
+
+install_r16() {
+	local currentfolder="$(pwd)"
+	jscodeshift --help || npm install -g jscodeshift
+	[[ -d "${currentfolder}/react-codemod" ]] || git clone https://github.com/reactjs/react-codemod.git react-codemod
+	[[ -d "${currentfolder}/react-codemod/node_modules" ]] || {
+		cd ./react-codemod &&
+		npm install && cd $currentfolder; echo ""; echo "------Install Success-------"
+	}
+}
+
+uninstall_r16() {
+	npm uninstall -g jscodeshift && 
+	rm -rf ./react-codemod && 
+	echo ""; echo "------Uninstall Success-------"
+}
+
+
+upgrade_in_batch() {
+	for pack in "${PACKPATH[@]}"
+	do
+		[[ -d ${pack} ]] || echo "Can not find ${pack}"
+		[[ -d ${pack} ]] && {
+			upgrade ${pack}
+		}
+	done
+}
+
+upgrade() {
+	local pack="${1}"
+	local currentfolder="$(pwd)"
+	echo "Start to upgrade: ${pack}"
+
+	for op in "${OPS[@]}"
+	do
+		echo "Start: ${op} ---> ${pack}"
+		jscodeshift -t "${currentfolder}${op}" ${pack}
+	done
+}
+
+install_r16 &&
+upgrade_in_batch
