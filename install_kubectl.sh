3658f48d288a935da999a10a65855539fa4ee7e6
Yi Lu <luyi985@gmail.com>
2025-12-28 12:18:31 +1100

    feat: fix k8s path

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -1,15 +1,22 @@
 #! /bin/bash
-## Method to install kubectl on Ubuntu
-# sudo apt-get update && 
-# sudo apt-get install -y apt-transport-https ca-certificates curl &&
-# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - &&
-# echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list &&
-# sudo apt-get update &&
-# sudo apt-get install -y kubectl&&
-# kubectl version --client
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectlcurl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# Detect OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    OS="darwin"
+    if [[ $(uname -m) == "arm64" ]]; then
+        ARCH="arm64"
+    else
+        ARCH="amd64"
+    fi
+else
+    # Linux
+    OS="linux"
+    ARCH="amd64"
+fi
+
+echo "Downloading kubectl for $OS/$ARCH..."
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/$OS/$ARCH/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
